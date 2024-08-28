154d7261485c21f4dcaf6eff14a2ad038f595688
Isaac Lyons <isaac@snowskeleton.net>
2024-08-28 14:07:12 -0400

    removed plist file, added value to project

--- OSTRich/OSTRichApp.swift
+++ OSTRich/OSTRichApp.swift
@@ -7,54 +7,29 @@
 
 import SwiftUI
 import SwiftData
+import Aptabase
 
 @main
 struct OSTRichApp: App {
     @UIApplicationDelegateAdaptor var appDelegate: AppDelegate
-//    @State var sharedModelContainer: ModelContainer = {
-//        let schema = Schema([
-//            Event.self,
-//            GameStateV2.self,
-//            DeckConstruction.self,
-//            Drop.self,
-//            Draft.self,
-//            EventFormat.self,
-//            Match.self,
-//            MatchResult.self,
-//            Player.self,
-//            Pod.self,
-//            Registration.self,
-//            Reservation.self,
-//            Round.self,
-//            Seat.self,
-//            Standing.self,
-//            Top8Draft.self,
-//            Team.self,
-//            ScoutingResult.self
-//        ])
-//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-//        
-//        do {
-//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-//        } catch {
-//            crashProtection()
-//            do {
-//                NSLog("Failed to load current schema and config. Cleraing and trying again")
-//                return try ModelContainer(for: schema, configurations: [modelConfiguration])
-//            } catch {
-//                fatalError("Could not create ModelContainer: \(error)")
-//            }
-//        }
-//    }()
     @Environment(\.scenePhase) private var scenePhase
+    
+    init() {
+        Aptabase.shared.initialize(appKey: "A-SH-8446942175") // 👈 this is where you enter your App Key
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
 //                .modelContainer(for: [Event.self])
                 .onChange(of: scenePhase) { _, newPhase in
-                    if newPhase == .inactive {
+                    switch newPhase {
+                    case .inactive:
                         UserDefaults.standard.setValue(0, forKey: "timesLaunchedWithoutSafeClose")
+                    case .active:
+                        Aptabase.shared.trackEvent("app_started")
+                    default:
+                        break
                     }
                 }
         }
