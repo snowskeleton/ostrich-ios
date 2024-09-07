004ae5c093cdb15e6dc19883b7c40a7f049fd5cf
Isaac Lyons <isaac@snowskeleton.net>
2024-09-07 18:18:49 -0400

    not using this right now. maybe ever?

--- OSTRich/Tabs/Player Stats/ScoutingHistoryByPlayerView.swift
+++ OSTRich/Tabs/Player Stats/ScoutingHistoryByPlayerView.swift
@@ -5,26 +5,26 @@
 //  Created by snow on 9/6/24.
 //
 
-
-import SwiftUI
-import SwiftData
-import Charts
-
-struct ScoutingHistoryByPlayerView: View {
-    @State var players: [LocalPlayer]
-    @State var format: String
-    
-    var body: some View {
-        List(players) { player in
-            NavigationLink {
-                List {
-                    ScoutingHistoryByFormatView(player: player, format: format)
-                }
-            } label: {
-                Text(player.safeName)
-                    .font(.body)
-            }
-            .padding(.vertical, 2)
-        }
-    }
-}
+//
+//import SwiftUI
+//import SwiftData
+//import Charts
+//
+//struct ScoutingHistoryByPlayerView: View {
+//    @State var players: [LocalPlayer]
+//    @State var format: String
+//    
+//    var body: some View {
+//        List(players) { player in
+//            NavigationLink {
+//                List {
+//                    ScoutingHistoryByFormatView(player: player, format: format)
+//                }
+//            } label: {
+//                Text(player.safeName)
+//                    .font(.body)
+//            }
+//            .padding(.vertical, 2)
+//        }
+//    }
+//}
